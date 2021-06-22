2b2d5a32ff9b0d95179aefb20c1f8a972e04163c
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-22 20:51:04 +0000

    completed the 'without' function - it will take two arrays ('source' and 'itemsToRemove'). It will return a new array that is 'source' but with all matches found in 'itemsToRemove' removed.

---
+++ without.js
@@ -0,0 +1,51 @@
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+const without = function (source, itemsToRemove) {
+
+  let filteredArray = [];
+
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      filteredArray.push(source[i])
+    }
+  }
+
+  return filteredArray;
+}
+
+
+//Test cases 
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3", 3], [1, 2, "3"])) // => ["1", "2"]
+console.log(without(['hello', 'my', 'name', 'is', 'Veronica', 1, 2, 3, '4'], [1, 4, 'hello']))
+
+let words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+words = ['good', 'morning', 'everyone'];
+assertArraysEqual(without(words, ['everyone']), words)  //should return 'assertion failed'
