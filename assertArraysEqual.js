09b0dda25f00a17b8ca606089b9ba2a7b716c581
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-22 20:48:05 +0000

    renamed the function, it was misnamed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,7 +14,7 @@ const eqArrays = function(array1, array2) {
 };
 
 
-const assertEqual = function (array1, array2) {
+const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
