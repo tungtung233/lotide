02470705f002ccc785666877afc4394296646504
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-23 22:29:59 +0000

    completed the findKeyByValue function - it takes the value of an object and returns the key

---
+++ findKeyByValue.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function (objectList, property) {
+  for (const [key, value] of Object.entries(objectList)) {
+    if (value === property) return(key)
+  } 
+}
+
+//Test conditions
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const ageOfPeople = {
+  Veronica: 25,
+  Edward: 33,
+  Nick: 27,
+  Raymond: 58,
+  Ruby: 57
+}
+
+assertEqual(findKeyByValue(ageOfPeople, 25), 'Veronica');
+assertEqual(findKeyByValue(ageOfPeople, 58), 'Raymond');
+assertEqual(findKeyByValue(ageOfPeople, 33), 'Edward');
+
