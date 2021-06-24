39218d8ddafb3ba535a189cf4bf33de248d04dc9
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-24 20:52:56 +0000

    completed the 'takeUntil' function - it goesreturns element of an array until it hits the stopping factor

---
+++ takeUntil.js
@@ -0,0 +1,51 @@
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+const takeUntil = function(array, callback) {
+  let newArray = [];
+  let counter = 0
+
+  array.forEach(element => {
+    !callback(element) && counter === 0 ? newArray.push(element) : counter++
+  })
+
+  return newArray
+}
+
+
+
+//Test condition
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+// console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
