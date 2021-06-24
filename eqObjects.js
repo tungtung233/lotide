733fa19b9032584c19c86327cc40e25672f27ff4
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-24 01:58:59 +0000

    refactored

--- eqObjects.js
+++ eqObjects.js
@@ -8,11 +8,11 @@ const assertEqual = function(actual, expected) {
 
 
 //Using an object's key, we find it's value
-const findValueByKey = function (objectList, property) {
+const findValueByKey = function(objectList, property) {
   for (const [key, value] of Object.entries(objectList)) {
-    if (key === property) return value
-  } 
-}
+    if (key === property) return value;
+  }
+};
 
 
 // Checks to see if two arrays are exactly alike
@@ -40,7 +40,7 @@ const eqObjects = function(object1, object2) {
   // checking that the keys are the same
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  };
+  }
 
   const object1Keys = Object.keys(object1).sort();
   const object2Keys = Object.keys(object2).sort();
@@ -54,16 +54,16 @@ const eqObjects = function(object1, object2) {
     if (object1Keys[i] !== object2Keys[i]) {
       return false;
     }
-  } 
+  }
 
   //checking that the values are the same -
   let object1Values = [];
   let object2Values = [];
 
   object1Keys.forEach(element => {
-    object1Values.push(findValueByKey(object1, element))
-    object2Values.push(findValueByKey(object2, element))
-  })
+    object1Values.push(findValueByKey(object1, element));
+    object2Values.push(findValueByKey(object2, element));
+  });
 
   // console.log('\n object1Values = ')
   // console.log(object1Values)
@@ -77,10 +77,9 @@ const eqObjects = function(object1, object2) {
   for (let i = 0; i < object1Values.length; i++) {
     if (Array.isArray(object1Values[i]) && Array.isArray(object2Values[i])) {
       if (!eqArrays(object1Values[i], object2Values[i])) {
-        return false
+        return false;
       }
-    }
-    else if (object1Values[i] !== object2Values[i]) {
+    } else if (object1Values[i] !== object2Values[i]) {
       return false;
     }
   }
