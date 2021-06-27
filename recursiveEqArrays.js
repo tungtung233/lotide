3bcb7519ece661e1c9a5e3bde25750210105ca87
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-27 20:34:41 +0000

    fixed a typo

--- recursiveEqArrays.js
+++ recursiveEqArrays.js
@@ -19,4 +19,4 @@ function recursiveEqArrays(actual, expected) {
 
 console.log(recursiveEqArrays([[2, 3], [4]], [[2, 3], [4]])) // true
 console.log(recursiveEqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
-console.log(recursiveEqArrays([[2, 3, [[[[1]]]]], [4]], [[2, 3, [[[[1]]]]], [4]])) // => false
+console.log(recursiveEqArrays([[2, 3, [[[[1]]]]], [4]], [[2, 3, [[[[1]]]]], [4]])) // => true
