69f0260591f779f475eb91c3480dee084422ac79
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-28 17:50:10 +0000

    refactored and dried up the 'eqArrays' function

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-
 const eqArrays = function(array1, array2) {
 
   if (array1.length !== array2.length) {
@@ -23,13 +13,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
