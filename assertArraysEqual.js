d65e197ca61009ee8de0fff48615c72bf3ac83a9
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-28 17:58:18 +0000

    refactored and dried up the 'assertArraysEqual' function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,4 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -21,3 +7,5 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
   }
 };
+
+module.exports = assertArraysEqual;
